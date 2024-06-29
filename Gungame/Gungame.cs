7f1c71031ffe0e51fdd643ec66a6d3f60e30dec7
GuyApooye <guy.montia@gmail.com>
2024-06-29 22:45:08 +0300

    Add project files.

---
+++ Gungame/Gungame.cs
@@ -0,0 +1,38 @@
+﻿using BepInEx;
+using CitrusLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Landfall.Network;
+using HarmonyLib;
+
+namespace Gungame
+{
+    public struct PluginInfo
+    {
+        public const string PLUGIN_GUID = "io.github.guyapooye.gungame";
+        public const string PLUGIN_NAME = "Gungame";
+        public const string PLUGIN_VERSION = "1.0.0";
+        
+    }
+
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Gungame : BaseUnityPlugin
+    {
+        public static CitLog GungameLog = new CitLog(PluginInfo.PLUGIN_NAME);
+
+        public static void DoPatching()
+        {
+            Harmony harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+            harmony.PatchAll();
+        }
+
+        private void Awake()
+        {
+            DoPatching();
+            GungameLog.Log("HELLO WORLD AND IDK IM JUST WRTING SHIT TO TEST IT OUT");
+        }
+    }
+}
---
+++ Gungame/Patcher.cs
@@ -0,0 +1,39 @@
+﻿using CitrusLib;
+using HarmonyLib;
+using Landfall.Network;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gungame
+{
+
+    public static class Patcher
+    {
+
+        [HarmonyPatch(typeof(ServerClient), nameof(ServerClient.SendGameStateChanged), new Type[] {typeof(GameState), typeof(byte[])})]
+        class GameStartListener
+        {
+            static void Prefix(GameState state, byte[] extraData = null)
+            {
+                ServerClient world = Citrus.World;
+                if (state == GameState.Flying)
+                {
+                    Gungame.GungameLog.Log("IOSAHJNDOIASNDOAISDNOAISDNAOISD FLYING GAMESTATE");
+                    GameSettings gameSettings = world.GameRoomReference.CurrentGameSettings;
+                    gameSettings.TimeBeforeFirstRing = 0;
+                    world.SpawnTheRingIn
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(GameSettings), nameof(GameSettings.))]
+        class GameSettingsPatch
+        {
+
+        }
+    }
+}
